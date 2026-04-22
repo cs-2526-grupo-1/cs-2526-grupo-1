9b061264e2f5a0df3273c1f7206ab5268001f45c
fuihfuefuiewn <danielbonachelamartinez@gmail.com>
2026-04-22 16:37:52 +0200

    test 7

--- src/test/java/es/codeurjc/e2e/TransferE2ETest.java
+++ src/test/java/es/codeurjc/e2e/TransferE2ETest.java
@@ -219,4 +219,32 @@ public class TransferE2ETest {
         login(E2ETestConstants.USER2_USERNAME, E2ETestConstants.USER2_PASSWORD);
         checkBalanceHasNotChanged(toAccount, E2ETestConstants.INITIAL_BALANCE_ACCOUNT2);
     }
+
+
+    @Test
+    public void test7_makeSuccessfulTransferBetweenUsers() {
+        String fromAccount = E2ETestConstants.ACCOUNT_1_CHECKING;
+        String toAccount = E2ETestConstants.ACCOUNT_2_CHECKING;
+        double amountToTransfer = 500.0;
+        
+        double expectedBalanceUser1 = E2ETestConstants.INITIAL_BALANCE_ACCOUNT1_CHECKING - amountToTransfer;
+        double expectedBalanceUser2 = E2ETestConstants.INITIAL_BALANCE_ACCOUNT2 + amountToTransfer;
+
+        simulateTransfer(fromAccount, toAccount, amountToTransfer);
+
+        wait.until(ExpectedConditions.urlContains(E2ETestConstants.PATH_DASHBOARD));
+        
+        String balanceUser1 = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.id(E2ETestConstants.ID_BALANCE_PREFIX + fromAccount))).getText();
+        assertThat(Double.parseDouble(balanceUser1)).isCloseTo(expectedBalanceUser1, within(0.01));
+
+        driver.findElement(By.id(E2ETestConstants.ID_LOGOUT_BUTTON)).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id(E2ETestConstants.ID_LOGIN_BUTTON)));
+
+        login(E2ETestConstants.USER2_USERNAME, E2ETestConstants.USER2_PASSWORD);
+
+        String balanceUser2 = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.id(E2ETestConstants.ID_BALANCE_PREFIX + toAccount))).getText();
+        assertThat(Double.parseDouble(balanceUser2)).isCloseTo(expectedBalanceUser2, within(0.01));
+    }
 }
